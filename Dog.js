ab3649fa7b70f25b790e0d1583c2bd32acc33e30
Anh Phuong <anh-phuong.nguyen-hoang@ant-tech.eu>
2019-06-07 05:06:20 +0700

    sayhi method for Dog

--- Dog.js
+++ Dog.js
@@ -1,3 +1,12 @@
-function Dog() {
+var chalk = require("chalk");
+
+function Dog(name) {
+  this.name = name;
   console.log('dog');
 }
+
+Dog.prototype.sayHi = function() {
+  console.log('Hi i am a dog, my name is ' + chalk.blue(this.name));
+}
+
+module.exports = Dog;
---
+++ index.js
@@ -0,0 +1,5 @@
+var Dog = require('./Dog');
+
+const dog = new Dog('Tom');
+
+dog.sayHi();
